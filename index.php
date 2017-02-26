0fba32b91e06b61cd3dd97dd7ab311b77193549f
MrEnlise <Mr.Enlise@yandex.ru>
2017-02-26 11:54:01 +0200

    NewFirstSite

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/main.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="Container-Top">Container - Top - Menu</div>
+        <div class="Container">Container 1</div>
+        <div class="Container">Container 2</div>
+        <div class="Container">Container 3</div>
+        <?php
+        echo "Hello world";
+        ?>
+    </body>
+</html>
